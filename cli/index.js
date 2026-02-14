81fb1dc401fd6cd5d4a8abfaaac25caaf1fcfec2
sudogetshivam <sorealgalaxy@gmail.com>
2026-02-14 22:53:35 +0530

    pasty-delete-code feature

---
+++ cli/index.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+const axios = require("axios");
+
+const BASE_URL = "https://pasty-api.onrender.com/api/auth";
+
+const args = process.argv.slice(2)
+const command = args[0]
+
+async function run(){
+    if(command === "save"){
+        const message = args.slice(1).join(" ");
+        if(!message){
+            console.error("Error:")
+            console.log("Please provide a message")
+            return;
+        }
+
+        try{
+            const res = await axios.post(`${BASE_URL}/send-message`,{
+                message: message,
+            });
+            console.log(res.data.data.code)
+        }catch(error){
+            console.error("Error in retrieving message")
+        }
+    }
+    else if(command === "get"){
+        const code = args[1];
+        if(!code){
+            console.log("Please provide a right code");
+            return;
+        }
+
+        try {
+
+            const res = await axios.post(`${BASE_URL}/retrieve-message`,{
+                code:code
+            }
+            );
+            console.log(res.data.data?.message || res.data.message)
+            
+        } catch (error) {
+            console.log("Error in retrieving in message")
+            
+        }
+    }
+    else{
+    console.log("Pasty CLI - Global Clipboard Tool");
+    console.log("Usage:");
+    console.log("  clip save <message>");
+    console.log("  clip get <code>");
+    }
+}
+
+run();
+
+
--- server/route/message.route.js
+++ server/route/message.route.js
@@ -1,8 +1,9 @@
-import { sendMessage,retrieveMessage } from "../controller/message.controller.js";
+import { sendMessage,retrieveMessage,deleteMessage } from "../controller/message.controller.js";
 import express from 'express'
 
 const router = express.Router()
 router.post('/send-message',sendMessage)
 router.post('/retrieve-message',retrieveMessage)
+router.post('/delete-message',deleteMessage)
 
 export default router
